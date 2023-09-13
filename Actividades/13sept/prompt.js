f053acb8c6cbfe5954d96655079967f7dc1b40d7
Bull <papipiro2003@gmail.com>
2023-09-13 17:38:27 -0300

    apuntes 9 septiembre

---
+++ Actividades/13sept/prompt.js
@@ -0,0 +1,5 @@
+let edad = prompt("cual es tu edad?", 0);
+alert(`tienes ${edad} bof`)
+
+let question = confirm("hola");
+alert(question);
---
+++ Actividades/13sept/variables.js
@@ -0,0 +1,11 @@
+let nombre = "Andres"
+let apellido = "Alberto"
+
+var edad 
+let ciudad
+
+edad = 29
+ciudad = "Osorno"
+
+const nombrecompleto = nombre + " " + apellido
+console.log(nombrecompleto);
