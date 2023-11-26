d355243eeea5edfb358c319d23640ff3a59ab009
Bull <papipiro2003@gmail.com>
2023-11-26 16:37:34 -0300

    Simplify the app.jsx code moving SideBar to component

--- mobileApp/frontend/App.jsx
+++ mobileApp/frontend/App.jsx
@@ -1,71 +1,11 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import Home from "./containers/Home";
-import CustomSideBar from "./components/CustomSideBar";
+import SideBar from "./components/CustomSideBar";
 import GameScreen from "./containers/GameScreen";
 import Search from "./containers/Search";
 
-import Icons from "react-native-vector-icons/FontAwesome";
-
 const Stack = createNativeStackNavigator();
-const Drawer = createDrawerNavigator();
-
-const SideBar = () => {
-  return (
-    <Drawer.Navigator
-      drawerContent={(props) => <CustomSideBar {...props} />}
-      screenOptions={{
-        headerShown: false,
-        drawerActiveBackgroundColor: "#56647b",
-        drawerActiveTintColor: "#fff",
-        drawerInactiveTintColor: "#2C3A4F",
-        drawerLabelStyle: {
-          fontSize: 16,
-        },
-      }}
-    >
-      <Drawer.Screen
-        name="Home"
-        component={Home}
-        options={{
-          drawerIcon: ({ color }) => (
-            <Icons name="home" size={22} color={color} style={{ width: 25 }} />
-          ),
-        }}
-      />
-      <Drawer.Screen
-        name="Login"
-        component={Home}
-        options={{
-          drawerIcon: ({ color }) => (
-            <Icons
-              name="sign-in"
-              size={22}
-              color={color}
-              style={{ width: 25 }}
-            />
-          ),
-        }}
-      />
-      <Drawer.Screen
-        name="Sign Up"
-        component={Home}
-        options={{
-          drawerIcon: ({ color }) => (
-            <Icons
-              name="user-plus"
-              size={22}
-              color={color}
-              style={{ width: 25 }}
-            />
-          ),
-        }}
-      />
-    </Drawer.Navigator>
-  );
-};
 
 export default function AppNavigation() {
   return (
--- mobileApp/frontend/components/CustomSideBar.jsx
+++ mobileApp/frontend/components/CustomSideBar.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import { View, Text, TouchableOpacity, Image } from "react-native";
+import { View, Image } from "react-native";
 import {
   DrawerContentScrollView,
   DrawerItemList,
 } from "@react-navigation/drawer";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import Home from "../containers/Home";
 
 import { styles } from "../Styles/CustomSideBar";
-// sign-out
+import Icons from "react-native-vector-icons/FontAwesome";
 
-export default function CustomSideBar({ ...props }) {
+function CustomSideBar({ ...props }) {
   return (
     <DrawerContentScrollView {...props} style={styles.container}>
       <View style={styles.logoContainer}>
@@ -19,5 +21,61 @@ export default function CustomSideBar({ ...props }) {
       </View>
       <DrawerItemList {...props} />
     </DrawerContentScrollView>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+export default function SideBar() {
+  return (
+    <Drawer.Navigator
+      drawerContent={(props) => <CustomSideBar {...props} />}
+      screenOptions={{
+        headerShown: false,
+        drawerActiveBackgroundColor: "#56647b",
+        drawerActiveTintColor: "#fff",
+        drawerInactiveTintColor: "#2C3A4F",
+        drawerLabelStyle: {
+          fontSize: 16,
+        },
+      }}
+    >
+      <Drawer.Screen
+        name="Home"
+        component={Home}
+        options={{
+          drawerIcon: ({ color }) => (
+            <Icons name="home" size={22} color={color} style={{ width: 25 }} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Login"
+        component={Home}
+        options={{
+          drawerIcon: ({ color }) => (
+            <Icons
+              name="sign-in"
+              size={22}
+              color={color}
+              style={{ width: 25 }}
+            />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Sign Up"
+        component={Home}
+        options={{
+          drawerIcon: ({ color }) => (
+            <Icons
+              name="user-plus"
+              size={22}
+              color={color}
+              style={{ width: 25 }}
+            />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
   );
 }
